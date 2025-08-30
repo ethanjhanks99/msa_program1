5befa7b9d5a23e513f37ddf320cfbd3eeabd81ed
Ethan Hanks <ethanjhanks99@gmail.com>
2025-08-30 17:25:15 -0600

    Greedy matching

--- matches.py
+++ matches.py
@@ -50,11 +50,11 @@ class Proposer(Person):
         Person.__init__(self, name, priorities)
         self.proposalIndex = 0  # next person in our list to whom we might propose
 
-    def nextProposal(self):
+    def nextProposal(self) -> int:
         if self.proposalIndex >= len(self.priorities):
             #print('returned None')
             return None
-        goal = self.priorities[self.proposalIndex]
+        goal: int = self.priorities[self.proposalIndex]
         self.proposalIndex += 1
         return goal
 
@@ -78,7 +78,7 @@ class Proposee(Person):
         for rank in range(len(priorities)):
             self.ranking[priorities[rank]] = rank
 
-    def evaluateProposal(self, suitor):
+    def evaluateProposal(self, suitor: str) -> bool:
         """
         Evaluates a proposal, though does not enact it.
 
@@ -92,10 +92,23 @@ class Proposee(Person):
                 return True
             else:
                 return False
-        else:
-            return False
+        return False
+    
+    def evaluateGreedily(self, suitor: int) -> bool:
+        """
+        Evaluates a proposal, though does not enact it.
 
-def parseFile(filename):
+        Suitor is the string identifier for the proposer who is proposing
+
+        returns True if the proposee is not matched, False otherwise
+        """
+        if suitor in self.ranking:
+            if self.partner == None:
+                self.rank = self.ranking[suitor] + 1
+                return True
+        return False
+
+def parseFile(filename: str) -> list[str]:
     """
     Returns a list of (name,priority_list) pairs.
     """
@@ -136,7 +149,7 @@ def printPairings(proposerPref: dict, proposees: dict, proposer: str, proposee: 
 
 
 
-def doMatch(msg: str,fileTuple: tuple, proposer: str, proposee: str):
+def doMatch(msg: str,fileTuple: tuple, proposer: str, proposee: str) -> void:
     """
     Performs Gale Shapley matching algorithm
 
@@ -206,17 +219,85 @@ def doMatch(msg: str,fileTuple: tuple, proposer: str, proposee: str):
     print("Final Pairings are as follows:")
     printPairings(proposerPref, proposees, proposer, proposee)
 
-def doGreedyMatch(msg: str, fileTuple: tuple, proposer: str, proposee: str):
-    print("TODO")
+def doGreedyMatch(msg: str, fileTuple: tuple, proposer: str, proposee: str) -> void:
+    """
+    Performs Greedy matching algorithm
+
+    Args:
+        msg (string): Message for before output
+        fileTuple (tuple): Contains the files that are being worked with, as well as bool for verbose option
+        proposer (string): Used to print out who is proposing
+        proposee (string): Used to print out who is being proposed to
+    """
+    print("\n\n"+msg+" working with files ", fileTuple)
+    proposerList = parseFile(fileTuple[0])
+    proposerPref = dict()
+    # each item in hr_list is a person and their priority list
+    for person, priority in proposerList:
+        # proposerPref[person[0]] = Proposer(person[0], person[1])
+        proposerPref[person] = Proposer(person, priority)
+    unmatched = list(proposerPref.keys())
+
+    # initialize dictionary of appllicants
+    proposeeList = parseFile(fileTuple[1])
+    proposees = dict()
+    # each item in proposeeList is a person and their priority list
+    for person, priority in proposeeList:
+        proposees[person] = Proposee(person, priority)
+    verbose = fileTuple[2]
+    ############################### the real algorithm ##################################
+    while len(unmatched) > 0:
+        if verbose:
+            print("Unmatched " + proposer + "s", unmatched)
+        m = proposerPref[unmatched[0]]  # pick arbitrary unmatched employer
+        n = m.nextProposal()
+        if n is None:
+            if verbose:
+                print('No more options ' + str(m))
+            unmatched.pop(0)
+            continue
+        who = proposees[n]  # identify highest-rank applicant to which
+        #    m has not yet proposed
+        if verbose: print(m.name, 'proposes to', who.name)
+
+        if who.evaluateGreedily(m.name):
+            if verbose: print('  ', who.name, 'accepts the proposal')
+
+            if who.partner:
+                # previous partner is getting dumped
+                mOld = proposerPref[who.partner]
+                if verbose:
+                    print('  ', mOld.name, 'gets dumped')
+
+                mOld.partner = None
+                mOld.rank = 0
+                unmatched.append(mOld.name)
+
+            unmatched.pop(0)
+            who.partner = m.name
+            m.partner = who.name
+            m.rank = m.proposalIndex
+        else:
+            if verbose:
+                print('  ', who.name, 'rejects the proposal')
+
+        if verbose:
+            print("Tentative Pairings are as follows:")
+            printPairings(proposerPref, proposees, proposer, proposee)
+
+    # we should be done
+    print("Final Pairings are as follows:")
+    printPairings(proposerPref, proposees, proposer, proposee)
 
 files = [("Employers0.txt", "Applicants0.txt", True),
-         ("Employers.txt", "Applicants.txt", False),
-         ("Employers3.txt", "Applicants3.txt", False),
+         ("Employers.txt", "Applicants.txt", True),
+         ("Employers3.txt", "Applicants3.txt", True),
          # ("Employers1.txt", "Applicants1.txt", False),
          # ("Employers2.txt","Applicants2.txt", False)
         ]
 for fileTuple in files:
     doMatch("Employers propose ", fileTuple, "Employer", "Applicant")
+    doGreedyMatch("Employers greedy propose ", fileTuple, "Employer", "Applicant")
 
 
 
